35e6c4b583563adf9585e97367dc5a92ad5970e5
devyanx1210 <csoberano1210@gmail.com>
2026-04-13 14:13:24 +0800

    Changes

--- scripts/front.js
+++ scripts/front.js
@@ -2,4 +2,4 @@ const { execSync } = require('child_process')
 const run = (cmd) => execSync(cmd, { stdio: 'ignore' })
 try { run('git add .') } catch (_) {}
 try { run('git commit -m "Changes"') } catch (_) {}
-try { run('git checkout final_') } catch (_) {}
+try { run('git checkout finel') } catch (_) {}
